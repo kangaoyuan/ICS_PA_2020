a5cda35713a77081df8607844c78fd40e7df37f8
tracer-ics2020 <tracer@njuics.org>
2025-01-04 02:05:01 +0800

    >  compile 201402010101  Static hostname: can        Icon name: computer-laptop          Chassis: laptop 💻       Machine ID: 06c3734070b7405e8db24a5b903dca16          Boot ID: cdd37bb1b7c1472284c1e4b833d2b53d Operating System: Arch Linux           Kernel: Linux 6.12.7-arch1-1     Architecture: x86-64  Hardware Vendor: Dell Inc.   Hardware Model: Inspiron 5490 Firmware Version: 1.9.0    Firmware Date: Wed 2020-06-17     Firmware Age: 4y 6month 2w 4d  02:05:01 up 12:07,  1 user,  load average: 0.08, 0.17, 0.24

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -346,7 +346,7 @@ void test_expr() {
 
     // Here loop is hard code, corresponding to the arg of gen-expr tool.
     for (int i = 0; i < 10000; i++) {
-        if(i == 9999)
+        if(i == 1)
             printf("why?\n");
         bool     flag;
         unsigned ans, eval;
