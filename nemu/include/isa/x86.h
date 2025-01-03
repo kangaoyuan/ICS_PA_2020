9c75c95d2dcb175b0334afc0e933897daebcc1e6
tracer-ics2020 <tracer@njuics.org>
2025-01-03 17:05:43 +0800

    >  compile 201402010101  Static hostname: can        Icon name: computer-laptop          Chassis: laptop 💻       Machine ID: 06c3734070b7405e8db24a5b903dca16          Boot ID: cdd37bb1b7c1472284c1e4b833d2b53d Operating System: Arch Linux           Kernel: Linux 6.12.7-arch1-1     Architecture: x86-64  Hardware Vendor: Dell Inc.   Hardware Model: Inspiron 5490 Firmware Version: 1.9.0    Firmware Date: Wed 2020-06-17     Firmware Age: 4y 6month 2w 3d  17:05:43 up  3:08,  1 user,  load average: 0.29, 0.55, 0.37

--- nemu/include/isa/x86.h
+++ nemu/include/isa/x86.h
@@ -5,8 +5,8 @@
 
 
 // memory
-#define x86_IMAGE_START 0x100000
 #define x86_PMEM_BASE 0x0
+#define x86_IMAGE_START 0x100000
 
 // reg
 
@@ -18,20 +18,66 @@
  */
 
 typedef struct {
-  struct {
-    uint32_t _32;
-    uint16_t _16;
-    uint8_t _8[2];
-  } gpr[8];
+    union {
+        union {
+            uint32_t _32;
+            uint16_t _16;
+            uint8_t  _8[2];
+        } gpr[8];
 
-  /* Do NOT change the order of the GPRs' definitions. */
+        /* Do NOT change the order of the GPRs' definitions. */
 
-  /* In NEMU, rtlreg_t is exactly uint32_t. This makes RTL instructions
-   * in PA2 able to directly access these registers.
-   */
-  rtlreg_t eax, ecx, edx, ebx, esp, ebp, esi, edi;
+        /* In NEMU, rtlreg_t is exactly uint32_t. This makes RTL
+         * instructions in PA2 able to directly access these registers.
+         */
+        struct {
+            rtlreg_t eax, ecx, edx, ebx, esp, ebp, esi, edi;
+        };
+    };
 
-  vaddr_t pc;
+    vaddr_t pc;
+
+    union {
+        struct {
+            uint32_t CF : 1;
+            uint32_t resv1 : 1;
+            uint32_t PF : 1;
+            uint32_t resv2 : 1;
+            uint32_t AF : 1;
+            uint32_t resv3 : 1;
+            uint32_t ZF : 1;
+            uint32_t SF : 1;
+            uint32_t TF : 1;
+            uint32_t IF : 1;
+            uint32_t DF : 1;
+            uint32_t OF : 1;
+            uint32_t IOPL : 2;
+            uint32_t NT : 1;
+            uint32_t resv4 : 1;
+            uint32_t RF : 1;
+            uint32_t VM : 1;
+            uint32_t resv5 : 14;
+        };
+        uint32_t val;
+    } eflags;
+
+    uint32_t cs;
+    uint32_t ss;
+
+    bool INTR;
+    uint16_t TR;
+    uint32_t CR0;
+    uint32_t CR3;
+
+    struct {
+        uint32_t idtr_base;
+        uint16_t idtr_limit;
+    };
+
+    struct {
+        uint32_t gdtr_base;
+        uint16_t gdtr_limit;
+    };
 } x86_CPU_state;
 
 // decode
